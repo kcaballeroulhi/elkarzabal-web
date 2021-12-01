92c66c4f91394f05e0554412b25bb8adade94d2d
Aitor Dominguez Simon <aidominguez@birt.eus>
2021-12-01 12:25:31 +0100

    peticion todos los productos

---
+++ javascript/tablaProductos.js
@@ -0,0 +1,62 @@
+//CREAR LA PETICION DE LOS PRODUCTOS SEMANALES
+const URL = "http://localhost:3000/api/";
+var id = "";
+$.ajax({
+    url: URL + "product",
+    type: 'GET',
+    headers: { "Authorization": "Bearer " + localStorage.getItem('token') },
+    success: function (result) {
+        id = result.id;
+        console.log(id);
+        console.log(result)
+        /*document.getElementById("name").value = result.name;
+        document.getElementById("description").value = result.lastname;
+        document.getElementById("password").value = result.password;
+        document.getElementById("email").value = result.email;
+        document.getElementById("phone").value = result.phone;
+        document.getElementById("role").value = result.roleId;*/
+    },
+    error: function () {
+        alert("Revisa tu conexión");
+    }
+});
+
+
+$(document).ready(function () {
+    var cuerpoTabla = $('#cuerpoTablaPedido');
+
+    var tabla = $("<table></table>");
+    tabla.attr({
+        id: "tabla"
+    });
+
+    var tr = $("<tr><th id='cProducto'>Producto</th><th id='cDescripcion'>Descripción</th><th id='cPrecio'>Precio</th><th id='cStock'>Stock</th><th id='cCantidad'>Cantidad</th><th id='cAnadir'>Añadir producto</th></tr>");
+    tr.attr({
+        id: "columnas"
+    });
+
+    tabla.append(tr);
+
+    //RECORRER LOS PRODUCTOS
+    for (i in productos) {
+
+        var tr2 = $("<tr><td>"
+            +/*NOMBRE PRODUCTO*/ +"</td><td>"
+            +/*DESCRIPCIÓN*/ +"</td><td>"
+            +/*PRECIO*/ +"</td><td>"
+            +/*STOCK*/ +"</td><td>"
+            + "<input id='iCantidad' type='number' name='cantidad' min='1'/>" + "</td><td>"
+            + "<a href='' onclick=''><img id='iBoton' src='./images/add.svg'></a>" + "</td></tr>");
+        tr2.attr({
+            id: "celdas"
+        });
+
+        if (i % 2 == 0) {
+            tr2.css("background-color", "#E6EFD0");
+        }
+
+        tabla.append(tr2);
+    }
+
+    cuerpoTabla.append(tabla);
+})
