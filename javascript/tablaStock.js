980a6b3f6c0c474f7df3d3d1789faab62e24b6de
Aitor Dominguez Simon <aidominguez@birt.eus>
2022-01-20 19:26:28 +0100

    corregidos errores al añadir productos semanales

--- javascript/tablaStock.js
+++ javascript/tablaStock.js
@@ -5,18 +5,12 @@ var productos
 
 
 function addBBDD(id, stock, price) {
-    console.log("id " + id)
-    console.log("stock " + stock)
-
     $.ajax({
         method: "POST",
         url: URL + "weekly-product",
         headers: { "Authorization": "Bearer " + localStorage.getItem('token') },
         data: JSON.stringify({ "productId": id, "maxQuantity": stock, "price": price }),
         contentType: "application/json",
-        success: function (data) {
-            alert("añadido correctamente")
-        },
         error: function (e) {
             alert("se ha producido un error")
         }
@@ -26,15 +20,15 @@ function addBBDD(id, stock, price) {
 
 function update() {
 
-    var tab = document.getElementById("tabla");
-    var rows = tab.rows;
+    var checks = document.getElementsByClassName("chk")
 
-    for (var i = 1; i < rows.length; i++) {// recorre las filas de la tabla
-        var tag = "chk" + i;
-        var idTag = "id" + i;
-        var stockTag = "cantidad" + i;
-        var priceTag = "precio" + i;
-        if (document.getElementById(tag).checked) {
+    for (var j = 0; j < checks.length; j++) {
+        if (checks[j].checked == true) {
+            var id = checks[j].id
+            id = id.substring(3)
+            var idTag = "id" + id;
+            var stockTag = "cantidad" + id;
+            var priceTag = "precio" + id;
             var id = document.getElementById(idTag).value;
             var intID = parseInt(id);
             var stock = document.getElementById(stockTag).value;
@@ -44,8 +38,26 @@ function update() {
             addBBDD(intID, numberStock, numberPrice)
         }
     }
+    alert("añadido correctamente")
 }
 
+/*for (var i = 1; i < rows.length; i++) {// recorre las filas de la tabla
+    var tag = "chk" + i;
+    var idTag = "id" + i;
+    var stockTag = "cantidad" + i;
+    var priceTag = "precio" + i;
+    if (document.getElementById(tag).checked) {
+        var id = document.getElementById(idTag).value;
+        var intID = parseInt(id);
+        var stock = document.getElementById(stockTag).value;
+        var numberStock = parseFloat(stock);
+        var price = document.getElementById(priceTag).value;
+        var numberPrice = parseFloat(price)
+        addBBDD(intID, numberStock, numberPrice)
+    }
+}
+}
+*/
 
 $.ajax({
     async: false,
@@ -81,7 +93,7 @@ $(document).ready(function () {
         var productID = productos[i].id
         var imagen = productos[i].images[0]
         var tr2 = $("<tr><td>"
-            + '<input type="checkbox" id="chk' + productID + '"> </td><td>'
+            + '<input class = "chk"type="checkbox" id="chk' + productID + '"> </td><td>'
             + '<img src="' + imagen + '" alt="Girl in a jacket" width="100" height="100"></img>' + "</td><td>"
             + productos[i].name + "</td><td>"
             + productos[i].measurementUnit + "</td><td>"
