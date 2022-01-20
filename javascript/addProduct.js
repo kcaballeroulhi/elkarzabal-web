2c9d44792fae59498a95d6439c81df69babac792
Aitor Dominguez Simon <aidominguez@birt.eus>
2022-01-20 09:56:20 +0100

    obligamos a meter imagen

--- javascript/addProduct.js
+++ javascript/addProduct.js
@@ -2,37 +2,34 @@ function createProduct() {
     var name = document.getElementById("name").value;
     var description = document.getElementById("description").value;
     var measure = document.getElementById("measure").value;
-    var image = document.getElementById("imagen").value;
-    console.log(image)
+    console.log(imagen.files[0])
+
     if (imagen.files.length === 1) {
         var miProduct = new FormData();
         miProduct.append('name', name);
         miProduct.append('description', description);
         miProduct.append('measurementUnit', measure);
         miProduct.append('files', imagen.files[0])
+        $.ajax({
+            async: false,
+            type: 'POST',
+            url: URL + "product",
+            headers: { "Authorization": "Bearer " + localStorage.getItem('token') },
+            data: miProduct,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log(data)
+                alert(data.name + " añadido correctamente")
+            },
+            error: function (e) {
+                console.log(e)
+            }
+        });
     }
     else {
-        var miProduct = new FormData();
-        miProduct.append('name', name);
-        miProduct.append('description', description);
-        miProduct.append('measurementUnit', measure);
-    }
+        alert("tienes que añadir una imagen")
 
-    $.ajax({
-        async: false,
-        type: 'POST',
-        url: URL + "product",
-        headers: { "Authorization": "Bearer " + localStorage.getItem('token') },
-        data: miProduct,
-        contentType: false,
-        processData: false,
-        success: function (data) {
-            console.log(data)
-            alert(data.name + " añadido correctamente")
-        },
-        error: function (e) {
-            console.log(e)
-        }
-    });
+    }
 }
 
