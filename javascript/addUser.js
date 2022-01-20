e8579c90073982c0a8ea3c14190e5803e048a82a
Aitor Dominguez Simon <aidominguez@birt.eus>
2022-01-20 20:19:43 +0100

    verificado telefono

--- javascript/addUser.js
+++ javascript/addUser.js
@@ -10,29 +10,34 @@ function createNewUser() {
     var password = document.getElementById("password").value;
     var role = document.getElementById("role").value;
     var encryptPass = hex_sha1(password)
+    if (phone.length < 6) {
+        alert("El telefono tiene que tener al menos 6 digitos")
+    } else {
+        $.ajax({
+            method: "POST",
+            url: URL + "register",
+            headers: { "Authorization": "Bearer " + localStorage.getItem('token') },
+            data: JSON.stringify({
+                "name": name,
+                "lastname": lastname,
+                "phone": phone,
+                "email": email,
+                "password": encryptPass,
+                "roleId": parseInt(role),
+                "address": address,
+                "roleId": parseInt(role)
 
-    $.ajax({
-        method: "POST",
-        url: URL + "register",
-        headers: { "Authorization": "Bearer " + localStorage.getItem('token') },
-        data: JSON.stringify({
-            "name": name,
-            "lastname": lastname,
-            "phone": phone,
-            "email": email,
-            "password": encryptPass,
-            "roleId": parseInt(role),
-            "address": address,
-            "roleId": parseInt(role)
+            }),
+            contentType: "application/json",
+            success: function (data) {
+                alert("Usuario " + data.name + " " + data.lastname + " añadido correctamente")
+                window.location.replace('./deleteUser.html')
+            },
+            error: function (e) {
+                console.log(e)
+            }
+        });
+    }
 
-        }),
-        contentType: "application/json",
-        success: function (data) {
-            alert("Usuario " + data.name + " " + data.lastname + " añadido correctamente")
-            window.location.replace('./deleteUser.html')
-        },
-        error: function (e) {
-            console.log(e)
-        }
-    });
+
 }
